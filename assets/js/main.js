f1404994658c791f642ba22dbdd24fb2ad161f6f
Anbu <15264938+anburocky3@users.noreply.github.com>
2021-11-25 00:39:15 +0530

    Feature: Fixed navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,20 @@ navToggler.addEventListener("click", () => {
     icon.classList.toggle("hidden");
   });
 });
+
+function navbarFixed() {
+  const header_dom = document.querySelector(".header");
+  const nav_offset_top = header_dom.clientHeight + 50;
+
+  window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scroll >= nav_offset_top) {
+      header_dom.classList.add("navbar-fixed");
+    } else {
+      header_dom.classList.remove("navbar-fixed");
+    }
+  });
+}
+
+navbarFixed();
